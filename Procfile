22bb156a4b178cebb11828e86f21e25101661e0f
LevMaltcev5 <LevMaltcev5@mail.ru>
2017-08-08 15:53:04 +0300

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kompany.wsgi --log-file -
---
+++ kompany/settings_prod.py
@@ -0,0 +1,14 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = (
+    'default':{
+        'ENGINE': 'django.db.Backends.postgresql_psycopg2',
+        'NAME': 'db1',
+        'USER': 'lev',
+        'PASSWORD': 'dkhtdkjh7654321',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+)
